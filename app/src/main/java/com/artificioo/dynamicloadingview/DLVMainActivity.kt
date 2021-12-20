4daf500cbb8bd636bfa781c05dfdeda90100cab9
ntorassa_b2g <ntorassa@burntogive.com>
2021-12-20 09:51:02 -0300

    Activity intent-filter removed

--- app/src/main/java/com/artificioo/dynamicloadingview/DLVMainActivity.kt
+++ app/src/main/java/com/artificioo/dynamicloadingview/DLVMainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.artificioo.dynamicloadingview.view.DynamicLoadingView
 
-class MainActivity : AppCompatActivity() {
+class DLVMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
